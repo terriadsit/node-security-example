4afa277121da12dd9c6b091ae3ad3675e7531641
terriadsit <terriadsit@yahoo.com>
2022-10-05 17:13:51 -0500

    add serialize, deserialize

--- server.js
+++ server.js
@@ -32,10 +32,25 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
+// Save the session to the cookie
+// executes whenever userdata is being saved to the cookie
+// done is a callback in case we need to do any asynchronous work to serialize the user
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+// Read the session from the cookie
+// takes in object from our session and returns back the data that will
+// be made available inside of Express on the request.user
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+})
+
 const app = express();
 
 // secure endpoints with helmet, protect against common configuration issues
 app.use(helmet()); // all routes pass through helmet
+
 app.use(cookieSession({
     name: 'session',
     maxAge: 24 * 60 * 60 * 1000, // 1 day
@@ -43,6 +58,10 @@ app.use(cookieSession({
 }))
 app.use(passport.initialize()); //sets up passport session
 
+// middleware so Passport understands our cookie session and the request.use object
+// authenticates the session, validates everything signed as should be
+// allows deserialize function to be called and set req.user
+app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
     const isLoggedIn = true;
@@ -66,7 +85,7 @@ app.get('/auth/google/callback',
   passport.authenticate('google', { //middleware
     failureRedirect: '/failure' ,   // options for failing or successful authenticate
     successRedirect: '/',
-    session: false,
+    session: true,       // save session and serialize it in a cookie
   }), 
   (req, res) => {   // third parameter is request handler, could redirect here if desired vs above
     console.log('Google called us back!');
@@ -90,7 +109,7 @@ app.get('/', (req, res) => {
 });
 
 https.createServer({    //listen using https
-    key: fs.readFileSync('key.pem'), //certificate and key from OpenSSL
+    key: fs.readFileSync('key.pem'), //certificate and key from OpenSSL for secure server
     cert: fs.readFileSync('cert.pem'),
 }, app).listen(PORT, () => { 
 //app.listen(PORT, () => { // using express
