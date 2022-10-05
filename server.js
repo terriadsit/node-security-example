b7f9c06bb6e5a0a1eb1136cd98df3140d871e36a
terriadsit <terriadsit@yahoo.com>
2022-10-05 09:41:24 -0500

    edit error message

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ function checkLoggedIn(req, res, next) {
     const isLoggedIn = true;
     if (!isLoggedIn) {
         return res.status(401).json({
-            error: 'you must log in!',
+            error: 'You must log in!',
         })
     }
     next();
